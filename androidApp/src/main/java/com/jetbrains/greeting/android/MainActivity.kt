e261b98c86e30a063acbf035c2a5d48b0bef7807
thahir <ataherster@yahoo.co.id>
2024-09-19 11:26:45 +0700

    create your first KMP app

--- androidApp/src/main/java/com/jetbrains/greeting/android/MainActivity.kt
+++ androidApp/src/main/java/com/jetbrains/greeting/android/MainActivity.kt
@@ -3,11 +3,16 @@ package com.jetbrains.greeting.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.jetbrains.greeting.Greeting
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +24,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingView(Greeting().greet())
+                    //GreetingView(Greeting().greet())
+                    DefaultPreview()    
                 }
             }
         }
@@ -34,7 +40,22 @@ fun GreetingView(text: String) {
 @Preview
 @Composable
 fun DefaultPreview() {
-    MyApplicationTheme {
-        GreetingView("Hello, Android!")
+    //MyApplicationTheme {
+    //    GreetingView("Hello, Android!")
+    //}
+    MaterialTheme {
+        val greeting = remember {
+            Greeting().greet()
+        }
+
+        Column (
+            modifier = Modifier.padding(all = 20.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            greeting.forEach {
+                greeting -> Text(greeting)
+                Divider()
+            }
+        }
     }
 }
--- shared/src/commonMain/kotlin/com/jetbrains/greeting/Greeting.kt
+++ shared/src/commonMain/kotlin/com/jetbrains/greeting/Greeting.kt
@@ -5,8 +5,8 @@ import kotlin.random.Random
 class Greeting {
     private val platform: Platform = getPlatform()
 
-    fun greet(): String {
-        val firstWord = if (Random.nextBoolean()) "Hi!" else "Hello!"
-        return firstWord
+    fun greet(): List<String> = buildList {
+        add(if(Random.nextBoolean()) "Hi!" else "Hello!")
+        add("Guess what this is! > ${platform.name.reversed()}")
     }
 }
